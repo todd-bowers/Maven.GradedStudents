8b83b77e66d915f8d9bede91dd68e51aa017336b
todd-bowers <toddbows@gmail.com>
2022-07-04 20:08:18 -0400

    Getting started

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,15 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Student {
+    private String firstName;
+    private String lastName;
+    public ArrayList<Double> examScores;
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = new ArrayList<>();
+    }
+
 }
