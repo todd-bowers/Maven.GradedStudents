88e1fdb80a238abd9d9b4b151be4b6154d5fd79e
todd-bowers <toddbows@gmail.com>
2022-07-04 20:12:41 -0400

    Getters and setters

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -12,4 +12,13 @@ public class Student {
         this.examScores = new ArrayList<>();
     }
 
+    public String getFirstName() {return firstName;}
+
+    public void setFirstName(String firstName) {this.firstName = firstName;}
+
+    public String getLastName() {return lastName;}
+
+    public void setLastName(String lastName) {this.lastName = lastName;}
+
+    public Integer getNumberOfExamsTaken() {return examScores.size();}
 }
