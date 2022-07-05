a1e0ec94f39b763f09162271cae7fe787742248a
todd-bowers <toddbows@gmail.com>
2022-07-04 20:50:07 -0400

    pos and neg constructorTests added

--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,38 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StudentTest {
 
+    @Test
+    public void constructorTest() {
+        //Given
+        String firstName = "Todd";
+        String lastName = "Bowers";
+
+        //When
+        Student student = new Student(firstName, lastName);
+        String retrievedFirstName = student.getFirstName();
+        String retrievedLastName = student.getLastName();
+
+        //Then
+        Assert.assertEquals(firstName, retrievedFirstName);
+        Assert.assertEquals(lastName, retrievedLastName);
+    }
+    @Test
+    public void constructorTest2() {
+        //Given
+        String firstName = "Todd";
+        String lastName = "Bowers";
+
+        //When
+        Student student = new Student(firstName, lastName);
+        String retrievedFirstName = student.getFirstName();
+        String retrievedLastName = student.getLastName();
+
+        //Then
+        Assert.assertNotEquals(null, retrievedFirstName);
+        Assert.assertNotEquals(null, retrievedLastName);
+    }
 }
